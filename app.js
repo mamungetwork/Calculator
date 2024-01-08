67c429be6dc0878b8218aee01fa658dadf3446d6
mamungetwork <111362486+mamungetwork@users.noreply.github.com>
2024-01-08 21:46:10 +0600

    Calculator Done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use-strict";
 
 const displayBtn = document.querySelectorAll(".is_display");
+const allOperator = document.querySelectorAll(".operator");
 const displayCalculation = document.querySelector(".calculation");
 const displayResult = document.querySelector(".result");
 const cleanAllBtn = document.getElementById("c");
@@ -9,19 +10,35 @@ const equalBtn = document.getElementById("equal");
 const modulasBtn = document.getElementById("modulas");
 
 let isModulas = false;
+let isResult = false;
 let errorText;
 
+let specialChar = ["+", "-", "%", "×", "÷", "."];
 displayBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     let displayText = displayCalculation.textContent;
     let btnText = e.target.textContent;
     let btnValue = e.target.value;
-    showToDisplayText(displayText + btnText);
 
-    if (displayCalculation.clientWidth > 226) {
-      displayCalculation.style.fontSize = "28px";
-      displayCalculation.style.lineHeight = "30px";
+    let lastChar = displayText.slice(-1);
+    let isLastCharSpecial = specialChar.includes(lastChar);
+
+    if (!isResult) {
+      if (specialChar.includes(btnText) && isLastCharSpecial) {
+        let modifiedText = displayText.slice(0, -1) + btnText;
+        showToDisplayText(modifiedText);
+        return;
+      }
+      showToDisplayText(displayText + btnText);
+    } else {
+      let displayText = displayResult.textContent;
+      resultEdit();
+      showToDisplayText(displayText + btnText);
     }
+
+    displayCalculation.classList.remove("math_error");
+
+    textResize();
     if (displayCalculation.clientWidth > 258) {
       displayCalculation.style.wordBreak = "break-all";
     }
@@ -39,14 +56,23 @@ equalBtn.addEventListener("click", (e) => {
 cleanAllBtn.addEventListener("click", (e) => {
   cleanAllDisplay();
   hideToDisplayResult();
+  textResize();
 });
 
 backBtn.addEventListener("click", (e) => {
   backspaceHandle();
+  textResize();
 });
 
 modulasBtn.addEventListener("click", (e) => {
   isModulas = true;
+});
+allOperator.forEach((btn) => {
+  if (isResult) {
+    btn.addEventListener("click", (e) => {
+      resultEdit();
+    });
+  }
 });
 
 function showToDisplayText(text) {
@@ -58,8 +84,20 @@ function showToDisplayResult(result) {
     showToDisplayText("Error");
   } else {
     displayResult.style.display = "block";
-    displayResult.textContent = result;
     displayCalculation.classList.add("have_result");
+    isResult = true;
+    let isDecimalResult = result > Math.floor(result);
+    if (!isDecimalResult) {
+      displayResult.textContent = result;
+    } else if (isDecimalResult) {
+      let tempResult = parseFloat(result);
+      tempResult = tempResult.toString();
+      tempResult.length > 4
+        ? (tempResult = Number(tempResult).toFixed(2))
+        : tempResult;
+
+      displayResult.textContent = tempResult;
+    }
   }
 }
 
@@ -70,16 +108,24 @@ function hideToDisplayResult(result) {
 }
 
 function backspaceHandle() {
-  if (errorText) {
-    displayCalculation.textContent = "";
+  if (isResult) {
+    resultEdit();
   } else {
-    displayCalculation.textContent = displayCalculation.textContent.slice(
-      0,
-      -1
-    );
+    if (errorText && isModulas) {
+      displayCalculation.textContent = "";
+      isModulas = false;
+      errorText = false;
+    } else {
+      displayCalculation.textContent = displayCalculation.textContent.slice(
+        0,
+        -1
+      );
+    }
   }
 }
 function cleanAllDisplay() {
+  isModulas = false;
+  isResult = false;
   displayCalculation.textContent = "";
 }
 
@@ -97,8 +143,9 @@ function handleCalculation() {
     });
     theResult = new Function("return " + newArray.join(""))();
     showToDisplayResult(theResult);
+    displayCalculation.classList.remove("math_error");
   } catch (error) {
-    showToDisplayText("Error");
+    displayCalculation.classList.add("math_error");
     errorText = error;
   }
 }
@@ -116,6 +163,25 @@ function modulasHandle() {
     theResult = (a * c) / 100;
     showToDisplayResult(theResult);
   } else {
-    showToDisplayText("Error:Please follow this patteren 100%10");
+    showToDisplayText("Error: Please follow this patteren - 100%10");
+    displayCalculation.classList.add("is_error");
+    errorText = true;
+  }
+}
+
+function resultEdit() {
+  isResult = false;
+  let currentResult = displayResult.textContent;
+  displayCalculation.textContent = currentResult;
+  hideToDisplayResult();
+}
+
+function textResize() {
+  if (displayCalculation.textContent.length > 8) {
+    displayCalculation.style.fontSize = "28px";
+    displayCalculation.style.lineHeight = "30px";
+  } else {
+    displayCalculation.style.fontSize = "";
+    displayCalculation.style.lineHeight = "";
   }
 }
